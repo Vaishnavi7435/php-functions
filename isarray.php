08708ed9fecca2fe9777e5236bf2e497ee0adef1
Vaishnavi Subhash Dake <91066753+Vaishnavi7435@users.noreply.github.com>
2024-02-25 13:46:49 +0530

    Update isarray.php

--- isarray.php
+++ isarray.php
@@ -1,5 +1,5 @@
 <?php
-$isarray=array("Java", "C", "PHP", "Andriod", "Python", "HTML", "CSS", "JavaScript" );
-if($isarray)
-echo is_array($isarray)."is arraya";
+$isarray= array("Java", "C", "PHP", "Andriod", "Python", "HTML", "CSS", "JavaScript");
+//$isarray 
+echo is_array($isarray) ? "This is an array " : "This is not an array";
 ?>
