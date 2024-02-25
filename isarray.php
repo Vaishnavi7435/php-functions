cd45226d6b564f5e8042fa6a70524f49db5d6c6b
Vaishnavi Subhash Dake <91066753+Vaishnavi7435@users.noreply.github.com>
2024-02-25 13:48:40 +0530

    Update isarray.php

---
+++ isarray.php
@@ -0,0 +1,5 @@
+<?php
+$isarray= array("Java", "C", "PHP", "Andriod", "Python", "HTML", "CSS", "JavaScript");
+// $isarray is an array then it will return 1  
+echo is_array($isarray) ? "This is an array " : "This is not an array";
+?>
